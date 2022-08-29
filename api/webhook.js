29cdb5f3a135d388f7905002cae19cd47f6529eb
Oybek <atavullaev@list.ru>
2022-08-29 17:54:08 +0500

    telegram bot copier

--- api/webhook.js
+++ api/webhook.js
@@ -1,11 +1,16 @@
+const axios = require('axios');
+const TELEGRAM_TOKEN = '5748172921:AAHd3LaQzbaR_Psqych_iyHwrgTBnqYGIQw';
+
 export default function handler(request, response) {
-  response.status(200);
-  
+  response.status(200);  
   let data = '';
+
   request.on('data', chunk => {
     data += chunk;
   });
+
   request.on('end', () => {
-    response.send(JSON.parse(data));
+    data = JSON.parse(data).message;
+    axios.get(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=${data.chat.id}&text=${data.text}`);
   });
 }
